8b131c91bdeff219fbe77bcd38623e33053ac7c0
irfan maulana <irfanmaulana.pml@gmail.com>
2022-08-31 19:36:30 +0700

    add wait group

--- main.go
+++ main.go
@@ -3,24 +3,39 @@ package main
 import (
 	"assignment2/service"
 	"fmt"
+	"sync"
 )
 
 func main() {
 	var db []*service.User
 	userSvc := service.NewUserService(db)
 	names := []string{"Irfan", "Aulia", "Giva", "Fahmi", "Yusuf", "Taslim", "Burok"}
+
+	var wg1 sync.WaitGroup
+	wg1.Add(len(names))
+
 	for _, n := range names {
-		res := userSvc.Register(&service.User{Name: n})
-		fmt.Println(res)
+		go RegisterUser(n, userSvc, &wg1)
 	}
+	wg1.Wait()
 
 	resGet := userSvc.GetUser()
 	fmt.Println("-----------Hasil get user-------------")
+	var wg sync.WaitGroup
+	wg.Add(len(resGet))
 	for _, v := range resGet {
-		cetakNama(v.Name)
+		go cetakNama(&wg, v.Name)
 	}
+	wg.Wait()
 }
 
-func cetakNama(nama string) {
+func cetakNama(wg *sync.WaitGroup, nama string) {
 	fmt.Println(nama)
+	wg.Done()
+}
+
+func RegisterUser(name string, svc service.UserInterface, wg *sync.WaitGroup) {
+	res := svc.Register(&service.User{Name: name})
+	fmt.Println(res)
+	wg.Done()
 }
